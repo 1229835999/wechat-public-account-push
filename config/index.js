5c219b50b600b369966a5b1e6acda657e066752a
1229835999 <112268863+1229835999@users.noreply.github.com>
2022-08-28 18:12:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,27 +1,51 @@
 export const config = {
-    // 公众号配置
-    // 公众号appId
-    appId: "wx226788875630a30b",
-    // 公众号appSecret
-    appSecret: "031e40bc79932519035d1ce6b7d4ec86",
+    /**
+     * 公众号配置
+     */
+
+    // 公众号APP_ID
+    APP_ID: "wx226788875630a30b",
+    // 公众号APP_SECRET
+    APP_SECRET: "031e40bc79932519035d1ce6b7d4ec86",
     // 模板消息id
-    templateId: "vuCQiXgM-pCrU6AUdZhouMEXr2rYhiok0GIo5GvI__8",
+    TEMPLATE_ID: "vuCQiXgM-pCrU6AUdZhouMEXr2rYhiok0GIo5GvI__8",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["otAvt6pnxbOFzN5Y6oPrLmKvRmZ0", "otAvt6g-xJjAJ5qI0icJ0OvWZ5KM"],
+    USERS: ["otAvt6pnxbOFzN5Y6oPrLmKvRmZ0", "otAvt6g-xJjAJ5qI0icJ0OvWZ5KM"],
      
-    // 信息配置
+    /**
+     * 信息配置
+     */
+
+    /** 天气相关 */
+
     // 所在省份
-    province: "山西",
+    PROVINCE: "山西",
     // 所在城市
-    city: "晋中",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "灿灿", "year": "2003", "date": "02-18", "type": 'new'},
-      {"name": "瑞凯", "year": "2002", "date": "06-03", "type": 'new'},
+    CITY: "晋中",
+
+    /** 生日相关 */
+
+    // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
+    BIRTHDAYS: [
+      {"name": "灿灿", "year": "2003", "date": "02-18"},
+      {"name": "瑞凯", "year": "2002", "date": "06-03"},
     ],
+
+    /** 日期相关 */
+
     // 在一起的日子，格式同上
-    loveDate: "2022-04-07"
-    
+    LOVE_DATE: "2022-04-07",
+    // 结婚纪念日
+    MARRY_DATE: "2020-01-04",
+
+
+    /** 好文节选 */
+
+    // 好文节选的内容类型
+    // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+    LITERARY_PREFERENCE: ""
+
+
     }
 
 // {{date.DATA}}  
@@ -30,7 +54,9 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
+Footer
